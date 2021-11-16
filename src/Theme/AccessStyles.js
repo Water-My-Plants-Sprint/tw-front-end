10e2ef340d7959902b289bee73d215247c0b56b4
BunniMK <86545296+BunniMK@users.noreply.github.com>
2021-11-16 09:25:42 -0700

    Changed accessability page, dl'd styles, added styled componentnents

---
+++ src/Theme/AccessStyles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+
+const AccessStyles = styled.div`
+
+`
+
+export default AccessStyles;
--- src/components/Accessibility.js
+++ src/components/Accessibility.js
@@ -1,3 +1,52 @@
-import { Link } from "react-router-dom"
+import React, { useState, Fragment } from "react";
+import { useParams } from "react-router-dom";
+import { Dialog, Transition } from "@headlessui/react";
+import accessStyles from '../Theme/AccessStyles'
 
-<Link to = "/" />
+import userinfo from "../userinfo";
+import EditPlant from "./EditPlant";
+import EditProfile from "./EditProfile";
+
+
+
+
+function Accessibility (props) {
+    const [profile, setProfile] = useState(userinfo);
+    const [open, setOpen] = useState(false);
+    
+    const handleEdit = (profile) => {
+      setOpen(false);
+      // axiosWithAuth()
+      // .put(`website.com/api/login/${ID}`, profile)
+      // .then((res) => {
+      //   setProfile(res.data);
+      // })
+      // .catch((err) => {
+      //   console.log(err);
+      // });
+    };
+  
+    const handleEditSelect = () => {
+      setOpen(true);
+    };
+
+    return (
+        <div className="accessAll">
+         <div className="profile">
+          <img src={profile.imageUrl} alt=""/>
+            <div className="">
+              <h3>Name: {profile.name}</h3>
+              <h3>Phone: {profile.phoneNumber}</h3>
+              <button
+                  type="submit"
+                  id="submit"
+                  onClick={handleEditSelect}
+              />
+            </div>  
+         </div> 
+        </div>
+    );
+    
+};
+
+export default Accessibility;
