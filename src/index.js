06e760d995dfa9a660db26799b72e7e936ce26e4
Matt <86365526+matthewstack@users.noreply.github.com>
2021-11-16 16:05:51 -0500

    Fixed node modules and dependencies

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { render } from "react-dom";
 import App from "./App";
 
-import { ThemeProvider } from 'styled-components';
-import theme from '../src/Theme/index';
+import { ThemeProvider } from "styled-components";
+import theme from "../src/Theme/index";
 
 render(
   <ThemeProvider theme={theme}>
-  <Router>
-    <App />
-  </Router>
+    <Router>
+      <App />
+    </Router>
   </ThemeProvider>,
   document.getElementById("root")
 );
